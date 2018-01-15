9af3299d93a5ca7d39dc6450e011da9408885b35
wowkalucky <wowkalucky@gmail.com>
2018-01-15 16:41:21 +0200

    Make Azure intro video as default

--- openedx/core/djangoapps/models/course_details.py
+++ openedx/core/djangoapps/models/course_details.py
@@ -64,7 +64,7 @@ class CourseDetails(object):
         self.intro_video_captions = ''
         self.intro_video_id = ''
         self.intro_video_manifest = ''
-        self.intro_video_source = 'youtube'
+        self.intro_video_source = 'azure' if settings.FEATURES.get('ENABLE_VIDEO_UPLOAD_PIPELINE') else 'youtube'
         self.effort = None  # hours/week
         self.license = "all-rights-reserved"  # default course license is all rights reserved
         self.course_image_name = ""
