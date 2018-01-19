b9b78b4bf79047ba8092ad4bcfdd85a6dc77bfb8
Oksana Slusarenro <oksana.slu@gmail.com>
2018-01-19 17:41:56 +0200

    Fix js-quality, tests and add list is the official standard for language names for all Microsoft products

--- cms/static/js/views/previous_video_upload.js
+++ cms/static/js/views/previous_video_upload.js
@@ -78,13 +78,18 @@ define(
             },
 
             removeVideo: function(event) {
-                var videoView = this;
+                var videoView = this,
+                    message = gettext('Removing a video from this list does not affect course content. Any content that uses a previously uploaded video ID continues to display in the course.');  // eslint-disable-line max-len
 
                 event.preventDefault();
 
+                if (this.storageService === 'azure') {
+                    message = gettext('Removing a video from this list does not affect course content. This will not delete the video file from Azure storage and it will continue to play if included in a course unit. You can update the video or remove the URL link from the course if needed.');  // eslint-disable-line max-len
+                }
+
                 ViewUtils.confirmThenRunOperation(
                     gettext('Are you sure you want to remove this video from the list?'),
-                    gettext('Removing a video from this list does not affect course content. Any content that uses a previously uploaded video ID continues to display in the course.'),  // eslint-disable-line max-len
+                    message,
                     gettext('Remove'),
                     function() {
                         ViewUtils.runOperationShowingMessage(
