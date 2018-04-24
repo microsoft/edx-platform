132d43c151e38b32ff42e26811836ef5f6cf3b3f
btelnes <btelnes@microsoft.com>
2018-04-24 08:34:49 -0700

    Update aws.py

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -131,8 +131,6 @@ LMS_ROOT_URL = ENV_TOKENS.get('LMS_ROOT_URL')
 SITE_NAME = ENV_TOKENS['SITE_NAME']
 
 ALLOWED_HOSTS = [
-    # TODO: bbeggs remove this before prod, temp fix to get load testing running
-    "*",
     ENV_TOKENS.get('CMS_BASE')
 ]
 
